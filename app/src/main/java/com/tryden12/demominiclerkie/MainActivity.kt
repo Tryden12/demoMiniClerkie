e08b94593e3f71e318feeee009b945b9b2ca0a4f
Tyler Ryden <tryden11@gmail.com>
2022-05-12 15:05:57 -0500

    Erased if statement in MainActivity relating to fragment, fully setup a nav graph, and began construction of MainFragment

--- app/src/main/java/com/tryden12/demominiclerkie/MainActivity.kt
+++ app/src/main/java/com/tryden12/demominiclerkie/MainActivity.kt
@@ -8,10 +8,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.nav_host_fragment, MainFragment.newInstance())
-                .commitNow()
-        }
     }
 }
--- app/src/main/java/com/tryden12/demominiclerkie/ui/main/MainFragment.kt
+++ app/src/main/java/com/tryden12/demominiclerkie/ui/main/MainFragment.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.tryden12.demominiclerkie.R
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), View.OnClickListener {
 
+    var navController : NavController? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +20,20 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
+        view.findViewById<Button>(R.id.view_transactions_btn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.send_money_btn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.view_balance_btn).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
     }
 
 }
