c35be94dc3c172a889fcd3aa61b7bc177aa6731d
Tyler Ryden <tryden11@gmail.com>
2022-05-12 20:58:06 -0500

    added toggle menu

--- app/src/main/java/com/tryden12/demominiclerkie/MainActivity.kt
+++ app/src/main/java/com/tryden12/demominiclerkie/MainActivity.kt
@@ -2,11 +2,48 @@ package com.tryden12.demominiclerkie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.tryden12.demominiclerkie.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var toggle :ActionBarDrawerToggle
+    lateinit var binding: MainActivityBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open, R.string.close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.item1 -> Toast.makeText(applicationContext,
+                    "Clicked item 1", Toast.LENGTH_SHORT).show()
+                R.id.item2 -> Toast.makeText(applicationContext,
+                    "Clicked item 2", Toast.LENGTH_SHORT).show()
+                R.id.item3 -> Toast.makeText(applicationContext,
+                    "Clicked item 3", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
