2ec0c9cb67a2fb103fa845e89366231fd5c9df32
Tyler Ryden <tryden11@gmail.com>
2022-05-13 13:47:59 -0500

    added splash screen

---
+++ app/src/main/java/com/tryden12/demominiclerkie/ui/SplashScreen.kt
@@ -0,0 +1,19 @@
+package com.tryden12.demominiclerkie.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.tryden12.demominiclerkie.MainActivity
+
+
+@Suppress("DEPRECATION")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashScreen, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
