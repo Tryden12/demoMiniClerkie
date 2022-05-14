2d86962fcfa01f2a7d700bab2f5d2b47ee971c30
Tyler Ryden <tryden11@gmail.com>
2022-05-14 06:38:23 -0500

    initialized MainViewModel in MainActivity

--- app/src/main/java/com/tryden12/demominiclerkie/MainActivity.kt
+++ app/src/main/java/com/tryden12/demominiclerkie/MainActivity.kt
@@ -5,12 +5,17 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.tryden12.demominiclerkie.api.MyRetrofitBuilder.textWithImage
 import com.tryden12.demominiclerkie.databinding.MainActivityBinding
+import com.tryden12.demominiclerkie.viewmodels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var toggle :ActionBarDrawerToggle
     lateinit var binding: MainActivityBinding
+    lateinit var viewModel : MainViewModel
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,12 +23,11 @@ class MainActivity : AppCompatActivity() {
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        /***************** Toggle Menu ****************************************************/
         toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open, R.string.close)
         binding.drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
-
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
         binding.navView.setNavigationItemSelectedListener {
             when (it.itemId) {
                 R.id.action_menu1 -> Toast.makeText(applicationContext,
@@ -36,7 +40,17 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
+        /*********** View Model ****************************************************/
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel.textWithImage.observe(this, Observer { textWithImage
+            println("DEBUG: $textWithImage")
+        })
+        viewModel.setTextWithImageId("1")
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        viewModel.cancelJobs()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
