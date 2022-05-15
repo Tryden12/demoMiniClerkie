7578d25436b33cdb005732343cb36636983ee94c
Tyler Ryden <tryden11@gmail.com>
2022-05-14 21:25:03 -0500

    setup new adapter class and then setup recyclerview in mainfragment

--- app/src/main/java/com/tryden12/demominiclerkie/adapters/Adapter.kt
+++ app/src/main/java/com/tryden12/demominiclerkie/adapters/Adapter.kt
@@ -1,4 +1,55 @@
 package com.tryden12.demominiclerkie.adapters
 
-class Adapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import com.tryden12.demominiclerkie.R
+import com.tryden12.demominiclerkie.model.TextWithImage
+import kotlinx.android.synthetic.main.text_image_item.view.*
+
+class Adapter(private val context: Context?, private var textWithImageList: List<TextWithImage>?) :
+    RecyclerView.Adapter<Adapter.CustomViewHolder>() {
+
+    fun dataChanged(textWithImageList: List<TextWithImage>) {
+        this.textWithImageList = textWithImageList
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): CustomViewHolder {
+        val mView = LayoutInflater.from(context)
+            .inflate(R.layout.text_image_item, viewGroup, false)
+        return CustomViewHolder(mView)
+    }
+
+    override fun onBindViewHolder(customViewHolder: CustomViewHolder, pos: Int) {
+        val textWithImage = textWithImageList!![pos]
+        customViewHolder.title.text = textWithImage.title.toString()
+        customViewHolder.subtitle.text = textWithImage.subtitle.toString()
+
+         //Picasso.get().load(textWithImage.image).into(customViewHolder.image)
+
+
+    }
+
+    override fun getItemCount(): Int {
+        return if (textWithImageList == null) 0 else textWithImageList!!.size
+    }
+
+    inner class CustomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var image: ImageView
+        var title : TextView
+        var subtitle : TextView
+
+        init {
+            image    = itemView.findViewById(R.id.item_imageview)
+            title    = itemView.findViewById(R.id.item_title)
+            subtitle = itemView.findViewById(R.id.item_subtitle)
+        }
+
+    }
 }
--- app/src/main/java/com/tryden12/demominiclerkie/ui/main/MainFragment.kt
+++ app/src/main/java/com/tryden12/demominiclerkie/ui/main/MainFragment.kt
@@ -9,22 +9,31 @@ import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.tryden12.demominiclerkie.R
+import com.tryden12.demominiclerkie.adapters.Adapter
 import com.tryden12.demominiclerkie.adapters.MyAdapter
 import com.tryden12.demominiclerkie.common.Common
 import com.tryden12.demominiclerkie.databinding.FragmentMainBinding
-import kotlinx.android.synthetic.main.fragment_main.*
+import com.tryden12.demominiclerkie.model.TextWithImage
 import java.io.IOException
 import java.io.InputStream
 
 class MainFragment : Fragment(), View.OnClickListener {
 
-
     var navController : NavController? = null
     lateinit var mService : com.tryden12.demominiclerkie.api.TextWithImage
-    lateinit var adapter : MyAdapter
+
+    lateinit var adapter : Adapter
+    var my_recycler_view : RecyclerView? = null
+    var textWithImageModelArrayList: MutableList<TextWithImage>? = ArrayList()
+    var textWithImageAdapter: Adapter? = null
+
+
+    lateinit var myAdapter : MyAdapter
     lateinit var dialog : AlertDialog
 
     private lateinit var binding: FragmentMainBinding
@@ -45,46 +54,42 @@ class MainFragment : Fragment(), View.OnClickListener {
 
         // Setup Layout Manager
         val layoutManager = LinearLayoutManager(context)
-        binding.myRecyclerView.layoutManager = layoutManager
-
+        binding.myRecyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        binding.myRecyclerView!!.itemAnimator = DefaultItemAnimator()
+        binding.myRecyclerView.adapter = Adapter(context,textWithImageModelArrayList)
         // Add divider
         val divider = DividerItemDecoration(
             context, layoutManager.orientation
         )
         binding.myRecyclerView.addItemDecoration(divider)
 
-        // Bind adapter to recycler view
-        adapter = MyAdapter()
-        binding.myRecyclerView.adapter = adapter
-
-
-        navController = Navigation.findNavController(view)
-
-        view.findViewById<Button>(R.id.view_fullscreen_btn).setOnClickListener(this)
-        view.findViewById<Button>(R.id.view_popup_btn).setOnClickListener(this)
-
 
         /************************ Read Json ******************************************************/
         //readJson()
+        //jsonFileFromLocally
 
 
 
-
-
+        /*****************************************************************************************/
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.view_fullscreen_btn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.view_popup_btn).setOnClickListener(this)
+        /*****************************************************************************************/
     }
 
 
-    fun loadJSONFile(): String? {
+    fun loadJSONFromAsset(): String? {
         var json: String? = null
         json = try {
-            val inputStream: InputStream = context?.getAssets()!!.open("yourfilename.json")
-            val size = inputStream.available()
-            val byteArray = ByteArray(size)
-            inputStream.read(byteArray)
-            inputStream.close()
-            String(byteArray)
-        } catch (e: IOException) {
-            e.printStackTrace()
+            val `is` =
+                requireContext().assets.open("textWithImage.json")
+            val size = `is`.available()
+            val buffer = ByteArray(size)
+            `is`.read(buffer)
+            `is`.close()
+            String(buffer)
+        } catch (ex: IOException) {
+            ex.printStackTrace()
             return null
         }
         return json
@@ -97,5 +102,36 @@ class MainFragment : Fragment(), View.OnClickListener {
             R.id.view_popup_btn -> navController!!.navigate(R.id.action_mainFragment_to_popupFragment)
         }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
+    fun loadJSONFile(): String? {
+        var json: String? = null
+        json = try {
+            val inputStream: InputStream = context?.getAssets()!!.open("textWithImage.json")
+            val size = inputStream.available()
+            val byteArray = ByteArray(size)
+            inputStream.read(byteArray)
+            inputStream.close()
+            String(byteArray)
+        } catch (e: IOException) {
+            e.printStackTrace()
+            return null
+        }
+        return json
+    }
+
+ */
+
 
 }
